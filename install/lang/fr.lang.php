7bcfc0a89f3d65f003f5c8f424ea97c2fcff1fc7
Yoshiyuki Asaba <y-asaba at pgfoundry.org>
2007-07-03 09:31:18 +0000

    Add French translation. Patch contributed by Guillaume Lelarge.

---
+++ install/lang/fr.lang.php
@@ -0,0 +1,57 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Message catalog in English
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: Permission to use, copy, modify, and distribute this software and
+ * its documentation for any purpose and without fee is hereby
+ * granted, provided that the above copyright notice appear in all
+ * copies and that both that copyright notice and this permission
+ * notice appear in supporting documentation, and that the name of the
+ * author not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior
+ * permission. The author makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * @author     Ryuma Ando <ando@ecomas.co.jp>
+ * @copyright  2003-2007		PgPool Global Development Group
+ * @version    CVS: $Id$
+ */
+
+$message = array(
+    'lang' => 'fr',
+    'strLang' => 'Français',
+    'strNext' => 'Suivant',
+    'strCheck' => 'Vérifier de nouveau',
+    'strDirectoryCheck' => 'Vérification des répertoires',
+    'strParameterCheck' => 'Configuration des paramètres',
+    'strFinish' => 'Terminer',
+    'strPgConfFile' => 'Fichier pgpool.conf',
+    'strPasswordFile' => 'Fichier des mots de passe',
+    'strPgmgtFile' => 'Fichier de configuration de pgpoolAdmin',
+    'strTempDir' => 'Répertoire des modèles',
+    'strPgpoolCommand' => 'Commande Pgpool',
+    'strPgpoolCommandOption' => 'Option de la commande Pgpool',
+    'strCmdC' => 'Efface le cache des requêtes',
+    'strCmdD' => 'Mode debug',
+    'strCmdM' => 'Mode stop',
+    'strCmdN' => 'Ne pas exécuter en mode démon',
+    'strCmdPgpoolFile' => 'pgpool.conf',
+    'strCmdPcpFile' => 'pcp.conf',
+    'strCmdDesc' => 'Vide, cette option est ignorée.',
+    'strPgpoolLogFile' => 'Journal Pgpool',
+    'strPcpDir' => 'Répertoire PCP',
+    'strPcpTimeout' => 'Délai d\'attente PCP',
+    'strPcpHostName' => 'Nom d\'hôte PCP',
+    'strPcpRefreshTime' => 'Temps de rafraichissement',
+    'msgCongratulations' => 'Félicitations. L\'outil d\'administration de Pgpool a été installé&nbsp;!',
+    'msgDeleteInstallDir' => '&nbsp;: merci de supprimer ce répertoire',
+    'msgToTop' => 'Haut de page'
+);
+
+?>
