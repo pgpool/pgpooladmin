1c9e0ece0a0120d08582ea4726e575c7228b7d65
Nozomi Anzai <anzai at sraoss.co.jp>
2010-07-22 05:19:36 +0000

    Add define() for the new start option "-D"

--- conf/pgmgt.conf.php
+++ conf/pgmgt.conf.php
@@ -4,12 +4,13 @@ define('_PGPOOL2_CONFIG_FILE', '/usr/local/etc/pgpool.conf');
 define('_PGPOOL2_PASSWORD_FILE', '/usr/local/etc/pcp.conf');
 define('_PGPOOL2_COMMAND', '/usr/local/bin/pgpool');
 define('_PGPOOL2_CMD_OPTION_C', '0');
+define('_PGPOOL2_CMD_OPTION_LARGE_D', '0');
 define('_PGPOOL2_CMD_OPTION_D', '0');
 define('_PGPOOL2_CMD_OPTION_M', 's');
 define('_PGPOOL2_CMD_OPTION_N', '0');
 define('_PGPOOL2_LOG_FILE', '/tmp/pgpool.log');
 define('_PGPOOL2_PCP_DIR', '/usr/local/bin');
 define('_PGPOOL2_PCP_HOSTNAME', 'localhost');
-define('_PGPOOL2_PCP_TIMEOUT', '');
+define('_PGPOOL2_PCP_TIMEOUT', '10');
 define('_PGPOOL2_STATUS_REFRESH_TIME', '0');
 ?>
