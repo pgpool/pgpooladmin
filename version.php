a7ffe6e2ed46bc0046751b66a5c396d824b7e229
Tatsuo Ishii <ishii at sraoss.co.jp>
2010-08-16 06:16:33 +0000

    Update version.

--- version.php
+++ version.php
@@ -19,10 +19,10 @@
  * is" without express or implied warranty.
  *
  * @author     Ryuma Ando <ando@ecomas.co.jp>
- * @copyright  2003-2009 PgPool Global Development Group
+ * @copyright  2003-2010 PgPool Global Development Group
  * @version    SVN: $Id$
  */
 
-$version = '3.0';
+$version = '3.0 beta1';
 
 ?>
