7bafd1fafffcc61a413906364600a0c0fad0a784
Yugo Nagata <nagata@sraoss.co.jp>
2014-04-21 13:50:17 +0900

    Fix a typo of title in installing

--- install/checkDirectory.php
+++ install/checkDirectory.php
@@ -61,7 +61,7 @@ if (!$error && $action == 'next') {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><? echo $message['strDirectoryCheck'] ?></title>
+<title><?php echo $message['strDirectoryCheck'] ?></title>
 <link href="../screen.css" rel="stylesheet" type="text/css" />
 </head>
   <body>
