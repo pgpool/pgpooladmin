f8af12cee229e8b2c1d00c5f8855a294cdf25cc9
Nozomi Anzai <anzai@sraoss.co.jp>
2014-10-10 10:22:09 +0900

    Prepare 3.4 alpha1.

--- version.php
+++ version.php
@@ -19,10 +19,10 @@
  * is" without express or implied warranty.
  *
  * @author     Ryuma Ando <ando@ecomas.co.jp>
- * @copyright  2003-2013 PgPool Global Development Group
+ * @copyright  2003-2014 PgPool Global Development Group
  * @version    SVN: $Id$
  */
 
-$version = '3.3.1';
+$version = '3.4 alpha1';
 
 ?>
